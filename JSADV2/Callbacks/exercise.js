74ce2565fb88dd22f58fcadc80d21f22f9faedf2
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-10-12 16:43:10 +0300

    callback

--- JSADV2/Callbacks/exercise.js
+++ JSADV2/Callbacks/exercise.js
@@ -1,1 +1,19 @@
 
+function printAsyncName(callback, name) {
+    const helloInterval = setInterval(() => {
+        callback();
+      clearInterval(helloInterval);
+  
+      const nameInterval = setInterval(() => {
+        console.log(name);
+        clearInterval(nameInterval);
+      }, 2000);
+    }, 1000);
+  }
+  
+  function callbackFunction() {
+    console.log('Hello');
+  }
+
+  printAsyncName(callbackFunction, "mert");
+  
