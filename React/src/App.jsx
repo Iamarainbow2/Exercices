edd911a7d5758ea0c453c5477b35cb237b24956f
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-10-24 21:54:33 +0300

    05

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,17 +3,18 @@ import './App.css'
 function App() {
   const hello = "Hello, World!"
 
-  function greed (name){
-
-    return <h1>Hello, {name}!</h1>
+  function add (a,b){
+    const sum = a + b
+    return <h2>{sum}</h2>
   }
 
   return (
     <>
       <h1>{hello}</h1>
-      {greed(<h1>Mert</h1>)}
+      {add(1, 2)}
     </>
   )
 }
 
 export default App
+
