3e016bde31e5b9f1d039f7b8e85bea5ba526c0f0
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-10-24 21:59:23 +0300

    07

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
+
 
 function App() {
   const hello = "Hello, World!"
 
-  function add (a,b){
-    const sum = a + b
-    return <h2>{sum}</h2>
+  function Hello (){
+    const msg = "Hello, World!"
+    return <h2>{msg}</h2>
   }
 
   return (
     <>
-      <h1>{hello}</h1>
-      {add(1, 2)}
+      {hello}
     </>
   )
 }
