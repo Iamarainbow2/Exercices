90c94030bfb20e4c54fe5c1bd88b2fdfbbc8e0aa
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-16 18:18:16 +0300

    conditions added

--- React/src/Components/Welcome.jsx
+++ React/src/Components/Welcome.jsx
@@ -4,12 +4,13 @@ import Age from "./Age";
 function Welcome(props) {
 
   const { firstName = 'Guest' ,age} = props;
+  const shouldRenderAge = firstName === 'John' && age > 18 && age < 65;
 
     return (
       <>
         <h2>Welcome, {firstName}!</h2>
        
-        {age > 18 ? <Age age={age}/> : <p>You are not 18 or older</p>}
+        {shouldRenderAge && <Age age={age}/>}
       </>
     )
     }
