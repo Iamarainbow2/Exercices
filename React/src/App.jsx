253e65ba614e5e8c33db7297c64fa45fb56c235a
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-17 01:25:58 +0300

    Counter made with custom hook

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import GithubUsers from './Components/GithunUsers';
+import useCounter from './Components/useCounter';
+const CounterComponent = () => {
+  const { count, increment, decrement, reset } = useCounter(0);
 
-const App = () => {
   return (
     <div>
-      <GithubUsers />
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
 
-export default App;
+export default CounterComponent;
