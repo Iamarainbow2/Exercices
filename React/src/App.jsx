fdd9b5db85165065178df6fe93ed50a4fc8c47f0
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-16 17:45:37 +0300

    new prop passed and used

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <Welcome />
+      <Welcome age={26}/>
       
       
     </>
--- React/src/Components/Welcome.jsx
+++ React/src/Components/Welcome.jsx
@@ -3,11 +3,12 @@
 
 function Welcome(props) {
 
-  const { firstName = 'Guest' } = props;
+  const { firstName = 'Guest' ,age} = props;
 
     return (
       <>
         <h2>Welcome, {firstName}!</h2>
+        <p>Your age is {age}</p>
       </>
     )
     }
