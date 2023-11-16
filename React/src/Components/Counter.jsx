7fb088e44ed9d02ab8a2a8a534b02100e8cb278e
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-16 22:42:02 +0300

    Counter inline styles added

---
+++ React/src/Components/Counter.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', margin: '20px' }}>
+      <h2 style={{ color: 'blue' }}>Counter: {count}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
