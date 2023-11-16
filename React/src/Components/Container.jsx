7cdea04eeabe185ea40b89e062ae68b9ab2c111e
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-17 00:13:49 +0300

    Collapse added to title container

--- React/src/Components/Container.jsx
+++ React/src/Components/Container.jsx
@@ -1,11 +1,22 @@
 
+import  { useState } from 'react';
 import './Container.css';
 
 const Container = ({ title, children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleToggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className="custom-container">
-      {title && <h2 className="container-title">{title}</h2>}
-      {children}
+    <div className={`custom-container ${collapsed ? 'collapsed' : ''}`}>
+      {title && (
+        <h2 className="container-title" onClick={handleToggleCollapse}>
+          {title}
+        </h2>
+      )}
+      {!collapsed && children}
     </div>
   );
 };
