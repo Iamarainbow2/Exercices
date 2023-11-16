ccfbbc239a7adfed5750cc5cfca8f3d94e7784c9
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-16 20:42:05 +0300

    Question answered

--- React/src/Components/MouseClicker.jsx
+++ React/src/Components/MouseClicker.jsx
@@ -3,8 +3,9 @@
 function MouseClicker({ name }) {
   
   const handleClick = (event) => {
-    
-    const buttonName = event.target.name || event.target.parentElement?.name;
+    //image is in a upper z index than the button, click event handler only runs the most top element so that element will be the image not the button 
+
+    const buttonName = event.target.name || event.target.src;
     if (buttonName) {
       console.log(`Element ${buttonName} clicked!`);
     }
@@ -16,7 +17,6 @@ function MouseClicker({ name }) {
         Click me!
         <img
           src="https://coursereport-production.imgix.net/uploads/school/logo/1247/original/glifo_develhope.jpeg?w=72&h=72"
-          name='image'
           alt="Button Image"
           style={{ width: '50px', height: '50px', marginLeft: '10px' }}
         />
