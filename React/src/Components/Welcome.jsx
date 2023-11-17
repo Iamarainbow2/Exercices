5a48c54bcfc5e935def3bd6c0edf76fcf66d4602
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-11-17 03:15:04 +0300

    Nav Links added

--- React/src/Components/Welcome.jsx
+++ React/src/Components/Welcome.jsx
@@ -1,12 +1,34 @@
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Welcome from './Welcome'
+import Counter from './Counter';
+import ShowGithubUser from './ShowGithubUser';
 
-const Welcome = ({ name }) => {
+const App = () => {
   return (
-    <div>
-      <h1>Welcome, {name}!</h1>
-      <p>This is your welcome message.</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+            <li>
+              <Link to="/users/octocat">Show Github User</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Welcome name="Ahmet" />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/users/:username" element={<ShowGithubUser />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
-export default Welcome;
+export default App;
