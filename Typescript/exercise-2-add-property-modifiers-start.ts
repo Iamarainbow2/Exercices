0ee82661855cce8d84438ba2a16896d3738a9284
Mert <32313852+Iamarainbow2@users.noreply.github.com>
2023-12-03 01:44:25 +0300

    Property-modifiers

--- Typescript/exercise-2-add-property-modifiers-start.ts
+++ Typescript/exercise-2-add-property-modifiers-start.ts
@@ -2,33 +2,37 @@
  * START: Follow the instructions below.
  */
 
-
-
-const countryPopulation: { code: string; population: number } = {
-    code: "NZ",
-    population: 5_135_300,
-};
-
-// Define a `Country` interface that describes the object in this variable.
-// Add the `Country` type as a type annotation for this variable.
+// Change the `Country` interface so the `population` property is optional.
+// This will fix the type error for `countryB`.
 
 interface Country {
     name: string;
     code: string;
-    population: number;
+    population?: number; 
 }
 
-const countryData: Country = {
+const countryA: Country = {
     name: "India",
     code: "IN",
     population: 1_352_642_280,
 };
 
-// Define a `Currency` type alias that describes the object in this variable.
-// Add the `Currency` type as a type annotation for this variable.
+const countryB: Country = {
+    name: "Italy",
+    code: "IT"
+};
+
+const countryC: Country = {
+    name: "Spain",
+    code: "ES",
+    population: 47_450_795,
+};
+
+// Change the `Currency` type alias so the `name` property cannot be changed.
+// This will cause a type error. Remove the code that now has a type error.
 
 type Currency = {
-    name: string;
+    readonly name: string; // Made the `name` property readonly
     code: string;
     symbol: string;
 };
@@ -39,6 +43,5 @@ const currencyData: Currency = {
     symbol: "€",
 };
 
-// ----
 
 export {};
