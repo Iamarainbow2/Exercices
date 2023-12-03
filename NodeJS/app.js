2035e105aa34cea34195dc19d85e4162d3e26c6d
Iamarainbow2 <32313852+Iamarainbow2@users.noreply.github.com>
2023-12-03 19:51:27 +0300

    Lucky Draw

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,14 +1,38 @@
-const fs = require('fs');
-
-
-const filePath = './example.txt';
-const fileContent = 'Hello, this is the content of the file!';
-
-
-fs.writeFile(filePath, fileContent, (err) => {
-    if (err) {
-        console.error('Error writing to file:', err);
-    } else {
-        console.log(`File "${filePath}" has been written successfully.`);
-    }
-});
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+  
+  luckyDraw('Joe')
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+  
+  luckyDraw('Caroline')
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+  
+  luckyDraw('Sabrina')
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+  
